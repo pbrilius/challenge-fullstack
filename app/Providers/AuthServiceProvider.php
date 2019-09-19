0bd76cec58b805b3ce39535a5f83a6643a4b8d9a
Povilas Brilius <pbrilius@gmail.com>
2019-09-19 14:54:32 +0300

    Telescope debugger & Passport oauth provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,18 +13,16 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
      * Register any authentication / authorization services.
-     *
-     * @return void
      */
     public function boot()
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes();
     }
 }
---
+++ resolution/implementation.md
@@ -0,0 +1,6 @@
+# Activity log
+
+Stored primary configuration
+
+Installing debugger helper - telescope - & reading scaffolding js-css domestic-science
+

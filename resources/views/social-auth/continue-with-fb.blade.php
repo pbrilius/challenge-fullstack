5d59c97b364a8cd7ac9ee4b69e779b8b3f98bb1b
Povilas Brilius <pbrilius@gmail.com>
2019-09-20 14:06:03 +0300

    Facebook app sdk implementation

--- resources/views/social-auth/continue-with-fb.blade.php
+++ resources/views/social-auth/continue-with-fb.blade.php
@@ -1,1 +1,4 @@
-<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="false"></div>
+<fb:login-button 
+scope="public_profile,email"
+onlogin="checkLoginState();">
+</fb:login-button>
--- resources/views/social-auth/facebook.blade.php
+++ resources/views/social-auth/facebook.blade.php
@@ -1,5 +1,31 @@
-    <div id="fb-root"></div>
-    test component actual file
-    @push('scripts')
-        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0"></script>
-    @endpush
+
+@push('scripts')
+
+    <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '{your-app-id}',
+            cookie     : true,
+            xfbml      : true,
+            version    : '{api-version}'
+          });
+            
+          FB.AppEvents.logPageView();   
+            
+        };
+      
+        (function(d, s, id){
+           var js, fjs = d.getElementsByTagName(s)[0];
+           if (d.getElementById(id)) {return;}
+           js = d.createElement(s); js.id = id;
+           js.src = "https://connect.facebook.net/en_US/sdk.js";
+           fjs.parentNode.insertBefore(js, fjs);
+         }(document, 'script', 'facebook-jssdk'));
+    
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+            });
+        }
+    </script>
+@endpush
